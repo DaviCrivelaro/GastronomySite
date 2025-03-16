1898ee7a8ab808fa511ffea1420fafa56e5087e0
Davi Crivelaro <102494440+CrivelaroCunha@users.noreply.github.com>
2025-03-16 12:06:48 -0300

    Commit 1 do projeto

---
+++ backend/src/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+
+async function main (){
+    const hostname = 'localhost';
+    const port = 3000;
+
+    const app = express()
+
+    app.use(express.json())
+    app.use(cors())
+
+    app.get('/', (req, res)=> {
+        res.send({
+            success: true,
+            statusCode: 200,
+            body: 'Welcome to MyGastronomy'
+        })
+    })
+    app.listen(port,() => {
+        console.log(`Server runnning on: http://${hostname}:${port}`)
+    })
+}
+
+main()
